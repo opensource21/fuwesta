e6eaea5eb394604f97578a79f4c0feb7ebd0db9b
niels <opensource21@gmail.com>
2014-09-18 12:02:34 +0200

    Understanding RememberMe.

--- fuwesta-sample/src/main/java/de/ppi/samples/fuwesta/frontend/HomeController.java
+++ fuwesta-sample/src/main/java/de/ppi/samples/fuwesta/frontend/HomeController.java
@@ -4,13 +4,16 @@
 
 package de.ppi.samples.fuwesta.frontend;
 
+import org.apache.shiro.SecurityUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 /**
  * Simple start-controller.
@@ -50,13 +53,31 @@ public class HomeController {
     }
 
     /**
-     * Shows the login-view.
+     * Show the login-view.
      * 
-     * @return the login-view.
+     * @param username optional the username
+     * @param password optional the password
+     * @param rememberMe optional the rememberMe flag
+     * @param model the Spring model
+     * @return the login-view-name
      */
+    //J-
     @RequestMapping(value = URL.LOGIN, method = { RequestMethod.GET,
             RequestMethod.POST })
-    public String loginPage() {
+    public String loginPage(
+            @RequestParam(value = "username", required = false) String username,
+            @RequestParam(value = "password", required = false) String password,
+            @RequestParam(value = "remember-me", required = false) Boolean rememberMe,
+            Model model) {
+        if (StringUtils.isEmpty(username)) {
+            model.addAttribute("username", SecurityUtils.getSubject()
+                    .getPrincipal());
+        } else {
+            model.addAttribute("username", username);
+        }
+        model.addAttribute("password", password);
+        model.addAttribute("rememberMe", rememberMe);
+
         return "example/login";
     }
 }
