4d0490664c529cf5f292c97b55771cb6130713c0
niels <opensource21@gmail.com>
2015-05-16 13:53:51 +0200

    Put the check for paginating into a separate class.

--- fuwesta-sample/src/test/java/de/ppi/samples/fuwesta/selophane/test/PostPaginatingIntegrationTest.java
+++ fuwesta-sample/src/test/java/de/ppi/samples/fuwesta/selophane/test/PostPaginatingIntegrationTest.java
@@ -1,15 +1,12 @@
 package de.ppi.samples.fuwesta.selophane.test;
 
-import java.util.List;
-
 import org.dbunit.dataset.DataSetException;
 import org.dbunit.dataset.IDataSet;
 import org.junit.Test;
 
 import de.ppi.samples.fuwesta.dbunit.dataset.TestData;
 import de.ppi.samples.fuwesta.frontend.URL;
-import de.ppi.samples.fuwesta.selophane.widget.PaginatingBar;
-import de.ppi.samples.fuwesta.selophane.widget.PaginatingBarButton;
+import de.ppi.samples.fuwesta.selophane.checks.PaginatingBarChecks;
 
 /**
  * Test for the post-page.
@@ -17,6 +14,10 @@ import de.ppi.samples.fuwesta.selophane.widget.PaginatingBarButton;
  */
 public class PostPaginatingIntegrationTest extends AbstractPostIntegrationTest {
 
+    /**
+     * The number of pages.
+     */
+    private static final int NUMBER_OF_PAGES = 18;
     /**
      * The pagesize.
      */
@@ -27,7 +28,7 @@ public class PostPaginatingIntegrationTest extends AbstractPostIntegrationTest {
      */
     @Override
     protected IDataSet getDataSet() throws DataSetException {
-        return TestData.createPostData(PAGE_SIZE * 20);
+        return TestData.createPostData(PAGE_SIZE * NUMBER_OF_PAGES);
     }
 
     /**
@@ -37,82 +38,31 @@ public class PostPaginatingIntegrationTest extends AbstractPostIntegrationTest {
     @Test
     public void testPostPaginating() {
         browser.getRelativeUrl(URL.Post.LIST);
-        checkTableAndPaginating(1);
+        softly.assertThat(postListPage.getTable().getRowCount()).isEqualTo(
+                PAGE_SIZE + 1);
+        PaginatingBarChecks.checkPaginating(softly,
+                postListPage.getPaginatingBar(), 1, NUMBER_OF_PAGES);
     }
 
     /**
      * Test if the table shows 100 entries.
      *
      */
+    @SuppressWarnings("boxing")
     @Test
     public void testPostPaginatingNext() {
         browser.getRelativeUrl(URL.Post.LIST);
-        for (int i = 2; i < 20; i++) {
+        for (int i = 2; i < NUMBER_OF_PAGES; i++) {
             postListPage.getPaginatingBar().getNext().click();
-            checkTableAndPaginating(i);
+            softly.assertThat(postListPage.getTable().getRowCount()).isEqualTo(
+                    PAGE_SIZE + 1);
+            PaginatingBarChecks.checkPaginating(softly,
+                    postListPage.getPaginatingBar(), i, NUMBER_OF_PAGES);
+            softly.assertThat(
+                    postListPage.getTable().getCellAtIndex(1, 1).getText())
+                    .as("Currentpage=%s, NumberOfPages=%s", i, NUMBER_OF_PAGES)
+                    .isEqualTo("Title " + ((i - 1) * PAGE_SIZE + 1));
         }
     }
 
-    /**
-     * Checks if the paginating is shown correct and that the table starts with
-     * the correct item and has the expected number of items.
-     *
-     * @param currentPage the number of the current page.
-     *
-     */
-    @SuppressWarnings("boxing")
-    private void checkTableAndPaginating(final int currentPage) {
-        final PaginatingBar paginatingBar = postListPage.getPaginatingBar();
-        softly.assertThat(postListPage.getTable().getRowCount()).isEqualTo(
-                PAGE_SIZE + 1);
-        softly.assertThat(paginatingBar.getNrOfButtons()).isEqualTo(9);
-        boolean isFirstPage = currentPage == 1;
-        boolean isLastPage = currentPage == 20;
-        softly.assertThat(paginatingBar.getFirst().isDisabled())
-                .as("Currentpage=%s", currentPage).isEqualTo(isFirstPage);
-        softly.assertThat(paginatingBar.getPrevious().isDisabled())
-                .as("Currentpage=%s", currentPage).isEqualTo(isFirstPage);
-        softly.assertThat(paginatingBar.getLast().isDisabled())
-                .as("Currentpage=%s", currentPage).isEqualTo(isLastPage);
-        softly.assertThat(paginatingBar.getNext().isDisabled())
-                .as("Currentpage=%s", currentPage).isEqualTo(isLastPage);
-        final List<PaginatingBarButton> buttons = paginatingBar.getAllButtons();
-
-        final int currentPageIndex;
-        final int startLabel;
-        if (currentPage < 3) {
-            startLabel = 1;
-            currentPageIndex = currentPage + 1;
-        } else if (currentPage > 18) {
-            startLabel = 16;
-            currentPageIndex = 6 + currentPage - 20;
-        } else {
-            startLabel = currentPage - 2;
-            currentPageIndex = 4;
-        }
-        for (int i = 2; i < buttons.size() - 2; i++) {
-            final PaginatingBarButton element = buttons.get(i);
-            softly.assertThat(element.getText()).isEqualTo(
-                    "" + (startLabel + i - 2));
-            if (i == currentPageIndex) {
-                softly.assertThat(element.isActive())
-                        .as("Currentpage=%s and i=%s", currentPage, i).isTrue();
-                softly.assertThat(element.isDisabled())
-                        .as("Currentpage=%s and i=%s", currentPage, i)
-                        .isFalse();
-            } else {
-                softly.assertThat(element.isDisabled())
-                        .as("Currentpage=%s and i=%s", currentPage, i)
-                        .isFalse();
-                softly.assertThat(element.isActive())
-                        .as("Currentpage=%s and i=%s", currentPage, i)
-                        .isFalse();
-            }
-
-        }
-        softly.assertThat(
-                postListPage.getTable().getCellAtIndex(1, 1).getText())
-                .as("Currentpage=%s", currentPage)
-                .isEqualTo("Title " + ((currentPage - 1) * PAGE_SIZE + 1));
-    }
 }
