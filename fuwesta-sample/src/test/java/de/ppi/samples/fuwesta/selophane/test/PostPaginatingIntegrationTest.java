59050e35213de48ae968c9af96cbf44b8e07e95f
niels <opensource21@gmail.com>
2015-05-16 14:01:27 +0200

    Added test for First, Last, Previous

--- fuwesta-sample/src/test/java/de/ppi/samples/fuwesta/selophane/test/PostPaginatingIntegrationTest.java
+++ fuwesta-sample/src/test/java/de/ppi/samples/fuwesta/selophane/test/PostPaginatingIntegrationTest.java
@@ -1,5 +1,7 @@
 package de.ppi.samples.fuwesta.selophane.test;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.dbunit.dataset.DataSetException;
 import org.dbunit.dataset.IDataSet;
 import org.junit.Test;
@@ -7,6 +9,7 @@ import org.junit.Test;
 import de.ppi.samples.fuwesta.dbunit.dataset.TestData;
 import de.ppi.samples.fuwesta.frontend.URL;
 import de.ppi.samples.fuwesta.selophane.checks.PaginatingBarChecks;
+import de.ppi.samples.fuwesta.selophane.widget.PaginatingBar;
 
 /**
  * Test for the post-page.
@@ -45,7 +48,7 @@ public class PostPaginatingIntegrationTest extends AbstractPostIntegrationTest {
     }
 
     /**
-     * Test if the table shows 100 entries.
+     * Test the next-button of {@link PaginatingBar}.
      *
      */
     @SuppressWarnings("boxing")
@@ -63,6 +66,61 @@ public class PostPaginatingIntegrationTest extends AbstractPostIntegrationTest {
                     .as("Currentpage=%s, NumberOfPages=%s", i, NUMBER_OF_PAGES)
                     .isEqualTo("Title " + ((i - 1) * PAGE_SIZE + 1));
         }
+    }
+
+    /**
+     * Test the last-button of {@link PaginatingBar}.
+     *
+     */
+    @Test
+    public void testPostPaginatingLast() {
+        browser.getRelativeUrl(URL.Post.LIST);
+        assertThat(postListPage.getPaginatingBar().getLast().isDisabled())
+                .isFalse();
+        postListPage.getPaginatingBar().getLast().click();
+        PaginatingBarChecks.checkPaginating(softly,
+                postListPage.getPaginatingBar(), NUMBER_OF_PAGES,
+                NUMBER_OF_PAGES);
+    }
+
+    /**
+     * Test the previous-button of {@link PaginatingBar}.
+     *
+     */
+    @SuppressWarnings("boxing")
+    @Test
+    public void testPostPaginatingPrevious() {
+        browser.getRelativeUrl(URL.Post.LIST);
+        postListPage.getPaginatingBar().getLast().click();
+        for (int i = NUMBER_OF_PAGES - 1; i > 1; i--) {
+            postListPage.getPaginatingBar().getPrevious().click();
+            softly.assertThat(postListPage.getTable().getRowCount()).isEqualTo(
+                    PAGE_SIZE + 1);
+            PaginatingBarChecks.checkPaginating(softly,
+                    postListPage.getPaginatingBar(), i, NUMBER_OF_PAGES);
+            softly.assertThat(
+                    postListPage.getTable().getCellAtIndex(1, 1).getText())
+                    .as("Currentpage=%s, NumberOfPages=%s", i, NUMBER_OF_PAGES)
+                    .isEqualTo("Title " + ((i - 1) * PAGE_SIZE + 1));
+        }
+    }
+
+    /**
+     * Test the first-button of {@link PaginatingBar}.
+     *
+     */
+    @Test
+    public void testPostPaginatingFirst() {
+        browser.getRelativeUrl(URL.Post.LIST);
+        assertThat(postListPage.getPaginatingBar().getLast().isDisabled())
+                .isFalse();
+        postListPage.getPaginatingBar().getLast().click();
+        assertThat(postListPage.getPaginatingBar().getFirst().isDisabled())
+                .isFalse();
+        postListPage.getPaginatingBar().getFirst().click();
+
+        PaginatingBarChecks.checkPaginating(softly,
+                postListPage.getPaginatingBar(), 1, NUMBER_OF_PAGES);
     }
 
 }
