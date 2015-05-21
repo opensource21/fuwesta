12260ab710d726e2695e56f38ff1b2147ef5968f
niels <opensource21@gmail.com>
2015-05-21 17:41:44 +0200

    Check the actions of the table.

--- fuwesta-sample/src/test/java/de/ppi/samples/fuwesta/selophane/test/PostIntegrationTest.java
+++ fuwesta-sample/src/test/java/de/ppi/samples/fuwesta/selophane/test/PostIntegrationTest.java
@@ -9,6 +9,7 @@ import org.junit.Test;
 import de.ppi.samples.fuwesta.dbunit.dataset.TestData;
 import de.ppi.samples.fuwesta.frontend.URL;
 import de.ppi.samples.fuwesta.selophane.base.AuthRule.Auth;
+import de.ppi.samples.fuwesta.selophane.widget.ActionTable;
 import de.ppi.samples.fuwesta.selophane.widget.PaginatingBar;
 
 /**
@@ -53,7 +54,7 @@ public class PostIntegrationTest extends AbstractPostIntegrationTest {
     }
 
     /**
-     * Test if the table shows 2 entries.
+     * Test if the table shows 2 entries and that the label are fine.
      *
      *
      */
@@ -61,21 +62,31 @@ public class PostIntegrationTest extends AbstractPostIntegrationTest {
     public void testPostList() {
         browser.getRelativeUrl(URL.Post.LIST);
         assertThat(browser.getCurrentRelativeUrl()).isEqualTo(URL.Post.LIST);
-        softly.assertThat(postListPage.getTable().getNrOfDataRows()).isEqualTo(
-                2);
-        softly.assertThat(postListPage.getTable().getNrOfDataColumns())
-                .isEqualTo(2);
-        softly.assertThat(postListPage.getPaginatingBar().getNrOfButtons())
-                .isEqualTo(5);
-        softly.assertThat(postListPage.getPaginatingBar().getFirst()).hasClass(
+        final ActionTable table = postListPage.getTable();
+        softly.assertThat(table.getNrOfDataRows()).isEqualTo(2);
+        softly.assertThat(table.getNrOfDataColumns()).isEqualTo(2);
+        final ActionTable.Row firstRow = table.getDataRows().get(0);
+        softly.assertThat(firstRow.getActions()).hasSize(4);
+        softly.assertThat(firstRow.getActions().get(0).getText()).isEqualTo(
+                "Show");
+        softly.assertThat(firstRow.getActions().get(1).getText()).isEqualTo(
+                "Edit");
+        softly.assertThat(firstRow.getActions().get(2).getText()).isEqualTo(
+                "Partialedit");
+        softly.assertThat(firstRow.getActions().get(3).getText()).isEqualTo(
+                "Delete");
+
+        final PaginatingBar paginatingBar = postListPage.getPaginatingBar();
+        softly.assertThat(paginatingBar.getNrOfButtons()).isEqualTo(5);
+        softly.assertThat(paginatingBar.getFirst()).hasClass(
                 PaginatingBar.CLASS_DISABLED);
-        softly.assertThat(postListPage.getPaginatingBar().getLast()).hasClass(
+        softly.assertThat(paginatingBar.getLast()).hasClass(
                 PaginatingBar.CLASS_DISABLED);
-        softly.assertThat(postListPage.getPaginatingBar().getPrevious())
-                .hasClass(PaginatingBar.CLASS_DISABLED);
-        softly.assertThat(postListPage.getPaginatingBar().getNext()).hasClass(
+        softly.assertThat(paginatingBar.getPrevious()).hasClass(
                 PaginatingBar.CLASS_DISABLED);
-        softly.assertThat(postListPage.getPaginatingBar().getButton("1"))
-                .hasNotClass(PaginatingBar.CLASS_DISABLED);
+        softly.assertThat(paginatingBar.getNext()).hasClass(
+                PaginatingBar.CLASS_DISABLED);
+        softly.assertThat(paginatingBar.getButton("1")).hasNotClass(
+                PaginatingBar.CLASS_DISABLED);
     }
 }
