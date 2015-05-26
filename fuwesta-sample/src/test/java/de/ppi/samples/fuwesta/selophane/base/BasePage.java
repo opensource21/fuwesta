658372f9141aebbdab8f78c2476b53e843943374
niels <opensource21@gmail.com>
2015-05-26 10:36:57 +0200

    Added possibility to reload a page.

--- fuwesta-sample/src/test/java/de/ppi/samples/fuwesta/selophane/base/BasePage.java
+++ fuwesta-sample/src/test/java/de/ppi/samples/fuwesta/selophane/base/BasePage.java
@@ -11,12 +11,25 @@ import org.selophane.elements.factory.api.ElementFactory;
 public class BasePage {
 
     /**
+     * The searchcontext of this page.
+     */
+    private final SearchContext searchContext;
+
+    /**
      *
      * Initiates an object of type BasePage.
      *
      * @param searchContext the {@link SearchContext} to find the elements.
      */
     public BasePage(SearchContext searchContext) {
+        this.searchContext = searchContext;
+        reload();
+    }
+
+    /**
+     * Reload the page, this is important if the weblements are chached.
+     */
+    public final void reload() {
         ElementFactory.initElements(searchContext, this);
     }
 
