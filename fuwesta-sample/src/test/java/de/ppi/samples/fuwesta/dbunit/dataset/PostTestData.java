c2116c56cdf6400f553a212a69180330a3018ccc
niels <opensource21@gmail.com>
2015-05-29 12:09:01 +0200

    Now the DB-Unit-Validation can handles variables.

--- fuwesta-sample/src/test/java/de/ppi/samples/fuwesta/dbunit/dataset/PostTestData.java
+++ fuwesta-sample/src/test/java/de/ppi/samples/fuwesta/dbunit/dataset/PostTestData.java
@@ -3,6 +3,7 @@ package de.ppi.samples.fuwesta.dbunit.dataset;
 import static de.ppi.samples.fuwesta.dbunit.rowbuilder.PostRowBuilder.newPost;
 import static de.ppi.samples.fuwesta.dbunit.rowbuilder.TagPostingsRowBuilder.newTagPostings;
 import static org.dbunit.dataset.builder.ObjectFactory.ts;
+import static org.dbunit.validator.Validators.*;
 
 import org.dbunit.dataset.DataSetException;
 import org.dbunit.dataset.IDataSet;
@@ -13,18 +14,24 @@ import org.dbunit.dataset.builder.DataSetBuilder;
  */
 public class PostTestData {
 
+    /** Name of the vairable to store the id of the new POST. */
+    private static final String VARIABLE_TO_STORE_POST_ID = "newPostId";
+
     @SuppressWarnings("boxing")
     public static IDataSet buildPostCreatedDataSet() throws DataSetException {
         IDataSet oldDataset = TestData.initWithSampleData();
 
         final DataSetBuilder b = new DataSetBuilder();
         b.addDataSet(oldDataset);
-        newPost().Id(103L).Version(0L)
+
+        newPost().Id(gt(102, VARIABLE_TO_STORE_POST_ID)).Version(0L)
                 .Content("This is an example text.\r\nIt contains newlines.")
                 .CreationTime(ts("2015-01-30 00:00:00.0")).Title("Test-Title1")
                 .UserId(11L).addTo(b);
-        newTagPostings().Tags(1L).Postings(103L).addTo(b);
-        newTagPostings().Tags(2L).Postings(103L).addTo(b);
+        newTagPostings().Tags(1L).Postings(nprop(VARIABLE_TO_STORE_POST_ID))
+                .addTo(b);
+        newTagPostings().Tags(2L).Postings(nprop(VARIABLE_TO_STORE_POST_ID))
+                .addTo(b);
         return b.build();
     }
 
@@ -35,13 +42,14 @@ public class PostTestData {
         final DataSetBuilder b = new DataSetBuilder();
         b.addDataSet(oldDataset);
         newPost()
-                .Id(103L)
+                .Id(gt(102, VARIABLE_TO_STORE_POST_ID))
                 .Version(1L)
                 .Content(
                         "This is an example text.\r\nIt contains newlines.Not really conntent.")
                 .CreationTime(ts("2015-03-30 00:00:00.0"))
                 .Title("Test-Title1N").UserId(12L).addTo(b);
-        newTagPostings().Tags(1L).Postings(103L).addTo(b);
+        newTagPostings().Tags(1L).Postings(nprop(VARIABLE_TO_STORE_POST_ID))
+                .addTo(b);
         return b.build();
     }
 
@@ -52,11 +60,12 @@ public class PostTestData {
 
         final DataSetBuilder b = new DataSetBuilder();
         b.addDataSet(oldDataset);
-        newPost().Id(103L).Version(2L)
+        newPost().Id(gt(102, VARIABLE_TO_STORE_POST_ID)).Version(2L)
                 .Content("This is an example text.\r\nIt contains newlines.")
                 .CreationTime(ts("2015-01-30 00:00:00.0")).Title("Test-Title1")
                 .UserId(12L).addTo(b);
-        newTagPostings().Tags(1L).Postings(103L).addTo(b);
+        newTagPostings().Tags(1L).Postings(nprop(VARIABLE_TO_STORE_POST_ID))
+                .addTo(b);
         return b.build();
     }
 
