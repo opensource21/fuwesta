b185e91db7722451735079510d2d6edbf89facf4
niels <opensource21@gmail.com>
2015-09-01 15:32:01 +0200

    Create a separate entry for the EVENT_STORAGE.

--- fuwesta-sample/src/test/java/de/ppi/samples/fuwesta/selophane/base/WebTestConstants.java
+++ fuwesta-sample/src/test/java/de/ppi/samples/fuwesta/selophane/base/WebTestConstants.java
@@ -20,17 +20,22 @@ public interface WebTestConstants {
     TestWebServer WEB_SERVER = new TestWebServer("/fuwesta");
 
     /**
+     * The system to store the events.
+     */
+    H2EventStorage EVENT_STORAGE = new H2EventStorage(
+            "jdbc:h2:./dbs/testlog;MODE=PostgreSQL;AUTO_SERVER=TRUE", "sa", "");
+
+    /**
      * Standard_Rule for WebTests.
      */
     RuleChain WEBTEST_WITHOUT_AUTHENTICATION = RuleChain
             .outerRule(new WebServerRule(new DelegatingWebServer(WEB_SERVER)))
-            .around(new EventLogRule(new H2EventStorage(
-                    "jdbc:h2:./dbs/testlog;MODE=PostgreSQL;AUTO_SERVER=TRUE",
-                    "sa", ""))).around(new WebDriverRule())
-            .around(new ProtocolRule("weblog"));
+            .around(new EventLogRule(EVENT_STORAGE))
+            .around(new WebDriverRule()).around(new ProtocolRule("weblog"));
     /**
      * Standard_Rule for WebTests.
      */
     RuleChain WEBTEST = RuleChain.outerRule(WEBTEST_WITHOUT_AUTHENTICATION)
             .around(new AuthRule());
+
 }
